5373a1066856ebc2293461359d78320fb09f47e6
Melissa <mawilliamson10@gmail.com>
2022-01-18 22:30:17 +0000

    Turns a spinner 1 revolution

---
+++ spinner1.js
@@ -0,0 +1,23 @@
+// process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1000);
+
